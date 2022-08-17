5555267e1603ef0a095ba84d1cf2f44d5034f9c6
Marjo R <93311776+MarianneliR@users.noreply.github.com>
2022-08-17 16:41:50 +0300

    Updated Harjoitus.py
    Tells who won / tie

--- Harjoitus.py
+++ Harjoitus.py
@@ -1,3 +1,4 @@
+
 import random
 
 list1 = "rock", "scissors", "paper"
@@ -7,3 +8,15 @@ item = input("rock, scissors, paper?\n")
 random_item = random.choice(list1)
 
 print(random_item)
+
+if item == "rock" and random_item == "paper" or item == "paper" and random_item == "scissors" or item == "scissors" \
+        and random_item == "rock":
+    print("Computer won!")
+
+if item == "rock" and random_item == "scissors" or item == "scissors" and random_item == "paper" or item == "paper" \
+        and random_item == "rock":
+    print("You won!")
+
+if item == random_item:
+    print("It's a tie!")
+    
